147033fe560683cbae4fbfde2bfaa67ad5e71f14
Steven Heinrich <steven.heinrich@gmail.com>
2014-06-24 09:39:27 -0400

    add stubb for planning

---
+++ app/components/rendering-engines/Stub/core.js
@@ -0,0 +1,14 @@
+define([
+    './map-openlayers-publisher',
+    './clustering/clustering',
+    './libs/openlayers-2.13.1/OpenLayers'
+], function(publisher, olClustering) {
+    // Setup context for storing the context of 'this' from the component's main.js 
+    var context;
+
+    var exposed = {
+        init: function(thisContext) {
+        }
+    };
+
+});
