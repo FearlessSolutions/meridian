ff343bca79055a2fbbde6fd5b5b8d5c3a0d83728
Steven Heinrich <steven.heinrich@gmail.com>
2014-06-25 09:42:43 -0400

    add require lib for openlayers

--- app/components/rendering-engines/map-openlayers/map/layers.js
+++ app/components/rendering-engines/map-openlayers/map/layers.js
@@ -1,10 +1,12 @@
-define([], function(){
+define([
+    './../libs/openlayers-2.13.1/OpenLayers'
+], function(){
     // Setup context for storing the context of 'this' from the component's main.js 
     var context;
 
     var exposed = {
         init: function(thisContext) {
-        	context = thisContext;
+            context = thisContext;
         }
     };
     return exposed;
