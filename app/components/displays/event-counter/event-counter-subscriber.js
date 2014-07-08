cbdb7973b9694ee77293a09a1caf427bb9265b8b
Steven Heinrich <steven.heinrich@gmail.com>
2014-07-08 15:56:33 -0400

    update counter to always retrive counts from datastorage

--- app/components/displays/event-counter/event-counter-subscriber.js
+++ app/components/displays/event-counter/event-counter-subscriber.js
@@ -6,10 +6,10 @@ define([
 	var exposed = {
         init: function(thisContext){
             context = thisContext;
-            context.sandbox.on('data.add', eventCountTool.addPoints);
-            context.sandbox.on('map.layer.hide', eventCountTool.hideLayer);
-            context.sandbox.on('map.layer.hide.all', eventCountTool.hideAll);
-            context.sandbox.on('map.layer.show', eventCountTool.showLayer);
+            context.sandbox.on('data.add', eventCountTool.update);
+            context.sandbox.on('map.layer.hide', eventCountTool.update);
+            context.sandbox.on('map.layer.hide.all', eventCountTool.update);
+            context.sandbox.on('map.layer.show', eventCountTool.update);
             context.sandbox.on('system.clear', eventCountTool.clear);
         }
     };	
--- app/components/displays/event-counter/event-counter.js
+++ app/components/displays/event-counter/event-counter.js
@@ -16,34 +16,17 @@ define([
             $displayedBadge = context.$('#stats-displayed-features.badge');
             $totalBadge = context.$('#stats-total-features.badge');
         },
-        addPoints: function(args){
-            var dataset = context.sandbox.dataStorage.datasets[args.queryId];
-            totalCount += args.data.length;
-            $totalBadge.html(totalCount);
-
-            if(dataset && dataset.visible){
-                displayedCount += args.data.length;
-                $displayedBadge.html(displayedCount);
-            }
-        },
-        showLayer: function(args){
-            var layer = context.sandbox.dataStorage.datasets[args.layerId];
-
-            if(layer){
-                displayedCount += layer.models.length;
-                $displayedBadge.html(displayedCount);
-            }
-        },
-        hideLayer: function(args){
-            var layer = context.sandbox.dataStorage.datasets[args.layerId];
-            if(layer){
-                displayedCount -= layer.models.length;
-                $displayedBadge.html(displayedCount);                
-            }
-        },
-        hideAll: function(){
+        update: function() {
+            totalCount = 0;
             displayedCount = 0;
+            context.sandbox.utils.each(context.sandbox.dataStorage.datasets, function(key, value) {
+                totalCount += value.length;
+                if(context.sandbox.stateManager.layers[key].visible) {
+                    displayedCount += value.length;
+                }
+            });
             $displayedBadge.html(displayedCount);
+            $totalBadge.html(totalCount);
         },
         clear: function(){
             totalCount = 0;
