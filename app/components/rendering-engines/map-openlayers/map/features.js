55c548fc5fdd3586d106ab284a9155470e1db31f
Steven Heinrich <steven.heinrich@gmail.com>
2014-07-08 16:40:11 -0400

    cleanup unused code

--- app/components/rendering-engines/map-openlayers/map/features.js
+++ app/components/rendering-engines/map-openlayers/map/features.js
@@ -17,33 +17,20 @@ define([
                 layer = params.map.getLayersBy('layerId', layerId)[0];
 
             if(layer) {
-                context.sandbox.utils.each(data, function(key, value) { // TODO: hoist and cleanup variables
+                context.sandbox.utils.each(data, function(key, value) {
                     
-
-
-                    // TODO: Test using GeoJSON Parser
                     // TODO: if GeoJSON is a feature collection split it into multiple individual GeoJSON features... to fit out process. May be a better fit to happen in the API, or could be here as a safety check.
                     var geoJsonParser = new OpenLayers.Format.GeoJSON({
-                        "ignoreExtraDims": false,
-                        "internalProjection": params.map.projection,
-                        "externalProjection": params.map.projectionWGS84
-                    }),
-                    currentFeature = geoJsonParser.parseFeature(value),
-                    iconData;
-            
-
-                    // TODO: Test without using GoeJSON Parser
-                    // if(value.geometry.type.toLowerCase() === 'point'){
-                    //     currentFeature = exposed.createPoint({
-                    //         "map": params.map,
-                    //         "geometry": value.geometry
-                    //     });
-                    // }
+                            "ignoreExtraDims": false,
+                            "internalProjection": params.map.projection,
+                            "externalProjection": params.map.projectionWGS84
+                        }),
+                        currentFeature = geoJsonParser.parseFeature(value),
+                        iconData;
                     
                     iconData = context.sandbox.icons.getIconForFeature({"properties": value.properties}) || context.sandbox.mapConfiguration.markerIcons.default;
                     currentFeature.featureId = value.id || '';
                     currentFeature.attributes.icon = iconData.icon;
-                    // currentFeature.attributes.graphicYOffset = iconData.graphicYOffset || 0;
                     currentFeature.attributes.height = iconData.height;
                     currentFeature.attributes.width = iconData.width;
                     currentFeature.attributes.dataService = value.dataService || '';
@@ -59,28 +46,7 @@ define([
                     "force": true,
                     "forces": true
                 });
-            } else {
-                // No layer exists, should we make a temp layer for them?
             }
-        },
-        createPoint: function(params) {
-            var newPoint = new OpenLayers.Feature.Vector(
-                new OpenLayers.Geometry.Point(
-                    params.geometry.coordinates[0], 
-                    params.geometry.coordinates[1]
-                ).transform(params.map.projectionWGS84, params.map.projection)
-            );
-
-            return newPoint;
-        },
-        createLine: function() {
-
-        },
-        createPolygon: function() {
-
-        },
-        removeFeatures: function(params) {
-
         }
     };
     return exposed;
