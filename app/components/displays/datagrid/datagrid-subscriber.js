a7d7f602883cd3109e606b2179935b838319827e
Steven Heinrich <steven.heinrich@gmail.com>
2014-08-12 16:05:54 -0400

    update datagrid to reload on hide/show of features and use new callback property for datatables to inject classes on rows

--- app/components/displays/datagrid/datagrid-subscriber.js
+++ app/components/displays/datagrid/datagrid-subscriber.js
@@ -12,6 +12,8 @@ define([
             context.sandbox.on('map.layer.create', datagrid.reload);
             context.sandbox.on('map.layer.delete', datagrid.reload);
             context.sandbox.on('map.features.plot', datagrid.reload);
+            context.sandbox.on('map.features.hide', datagrid.reload);
+            context.sandbox.on('map.features.show', datagrid.reload);
             context.sandbox.on('data.clear.all', datagrid.clear);
         }
     };
