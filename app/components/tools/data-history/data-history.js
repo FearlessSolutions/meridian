3e9854e8f8f6ef969ef92f8900f2e96441e72aec
Eric Herman <eherman1989@gmail.com>
2014-09-16 13:57:32 -0400

    Update data-history component to be more dynamic, but still use dummy data.

--- app/components/tools/data-history/data-history.js
+++ app/components/tools/data-history/data-history.js
@@ -1,19 +1,31 @@
 define([
     './data-history-publisher',
-    'bootstrap'
-], function (publisher) {
+    'text!./data-history-entry.hbs',
+    'text!./data-history-detail-view.hbs',
+    'bootstrap',
+    'handlebars'
+], function (publisher, dataHistoryEntryHBS, dataHistoryDetailViewHBS) {
     var context,
+        dataHistoryEntryTemplate,
+        dataHistoryDetailViewTemplate,
         MENU_DESIGNATION = 'data-history',
         $modal,
         $cancelButton,
-        $closeButton;
+        $closeButton,
+        $dataHistoryListTable,
+        $dataHistoryDetailView;
     
     var exposed = {
         init: function(thisContext) {
             context = thisContext;
+
+            dataHistoryEntryTemplate = Handlebars.compile(dataHistoryEntryHBS);
+            dataHistoryDetailViewTemplate = Handlebars.compile(dataHistoryDetailViewHBS);
+
             $modal = context.$('#data-history-modal');
-            $cancelButton = context.$('#data-history-cancel');
             $closeButton = context.$('#data-history-modal.modal button.close');
+            $dataHistoryListTable = context.$('#data-history-modal.modal .data-history-list');
+            $dataHistoryDetailView = context.$('#data-history-modal.modal .data-history-detail-view');
 
             $modal.modal({
                 "backdrop": true,
@@ -24,23 +36,63 @@ define([
                 exposed.hideDetailedInfo();
              });
 
-            $cancelButton.on('click', function(event) {
-                event.preventDefault();
-                publisher.closeDataHistory();
-            });
-
             $closeButton.on('click', function(event) {
                 event.preventDefault();
                 publisher.closeDataHistory();
             }); 
 
-            context.$('.data-action-info').on('click', function(event) {
-                exposed.showDetailedInfo();
-            });
+            context.$('.expiration span').tooltip();
+        },
+        open: function() {
+            publisher.publishOpening({"componentOpening": MENU_DESIGNATION});
+
+            // Populate Data History table
+            exposed.updateDataHistory();
+
+            $modal.modal('show');
+        },
+        close: function() {
+            $modal.modal('hide');
+        },
+        showDetailedInfo: function(dataObject) {
+            var datasetId = dataObject.datasetId;
+            console.debug(datasetId);
+
+            /**
+             * Get metadata from server for specific dataset by datasetId
+             */
+
+            // Dummy data that would be returned by server
+            var data = {
+                "datasetId": "123",
+                "dataName": "Test Dataset 1",
+                "dataDate": "Mon. 12/12/12 @12:00:00",
+                "dataRecordCount": "12,000",
+                "dataExpiresOn": "Mon. 01/12/12 @08:00:00",
+                "dataStatus": "Finished",
+                "rawDataObject": {
+                    "datasetId": "123",
+                    "dataName": "Test Dataset 1",
+                    "dataDate": "Mon. 12/12/12 @12:00:00",
+                    "dataRecordCount": "12,000",
+                    "dataExpiresOn": "Mon. 01/12/12 @08:00:00",
+                    "dataStatus": "Finished",
+                    "coords": {
+                        "lat": 0,
+                        "lon": 0
+                    }
+                }
+            };
+
+            var rawDataObjectString = JSON.stringify(data.rawDataObject, null, "  ");
+            data.rawDataObject = rawDataObjectString;
+
+            var dataHistoryDetailView = dataHistoryDetailViewTemplate(data);
+            $dataHistoryDetailView.html(dataHistoryDetailView);
+
             context.$('.data-history-modal-back-to-list').on('click', function(event) {
                 exposed.hideDetailedInfo();
             });
-
             context.$('.data-action-restore').on('click', function(event) {
                 var result = confirm("Want to restore this data?");
                 if(result === true) {
@@ -48,7 +100,6 @@ define([
                     publisher.closeDataHistory();
                 } 
             });
-
             context.$('.data-action-delete').on('click', function(event) {
                 var result = confirm("Want to delete?");
                 if(result === true) {
@@ -56,32 +107,88 @@ define([
                 } 
             });
 
-            context.$('.expiration span').tooltip();
-
-        },
-        open: function() {
-            publisher.publishOpening({"componentOpening": MENU_DESIGNATION});
-            $modal.modal('show');
-        },
-        close: function() {
-            $modal.modal('hide');
-        },
-        showDetailedInfo: function() {
-            context.$('.data-history-list-container').addClass('hidden');
-            context.$('.data-history-headers').addClass('hidden');
-            context.$('.data-history-modal-back-to-list').removeClass('hidden');
+            context.$('.data-history-summary-list-container').addClass('hidden');
             context.$('.data-history-detail-view').removeClass('hidden');
         },
         hideDetailedInfo: function() {
-            context.$('.data-history-list-container').removeClass('hidden');
-            context.$('.data-history-headers').removeClass('hidden');
-            context.$('.data-history-modal-back-to-list').addClass('hidden');
+            context.$('.data-history-summary-list-container').removeClass('hidden');
             context.$('.data-history-detail-view').addClass('hidden');
         },
         clear: function() {
             $modal.modal('hide');
+        },
+        updateDataHistory: function() {
+            // Clear previous data history list
+            $dataHistoryListTable.html('');
+
+            // Dummy data that would be returned by server
+            var data = [
+                {
+                    "datasetId": "123",
+                    "dataName": "Test Dataset 1",
+                    "dataDate": "Mon. 12/12/12 @12:00:00",
+                    "dataRecordCount": "12,000"
+                },
+                {
+                    "datasetId": "1234",
+                    "dataName": "Test Dataset 2",
+                    "dataDate": "Thurs. 11/11/11 @12:00:00",
+                    "dataRecordCount": "5,000"
+                },
+                {
+                    "datasetId": "12345",
+                    "dataName": "Test Dataset 3",
+                    "dataDate": "Tues. 10/10/10 @12:00:00",
+                    "dataRecordCount": "25,000"
+                }
+                // {
+                //     "queryId": "12345",
+                //     "dataName": "Test Query Dataset 3",
+                //     "dataDate": "Tues. 10/10/10 @12:00:00",
+                //     "dataRecordCount": "25,000"
+                // },
+                // {
+                //     "dataName": "Test Query Dataset 3",
+                //     "dataDate": "Tues. 10/10/10 @12:00:00",
+                //     "dataRecordCount": "25,000"
+                // },
+                // {
+                //     "dataName": "Test Query Dataset 3",
+                //     "dataDate": "Tues. 10/10/10 @12:00:00",
+                //     "dataRecordCount": "25,000"
+                // },
+                // {
+                //     "dataName": "Test Query Dataset 3",
+                //     "dataDate": "Tues. 10/10/10 @12:00:00",
+                //     "dataRecordCount": "25,000"
+                // },
+                // {
+                //     "dataName": "Test Query Dataset 3",
+                //     "dataDate": "Tues. 10/10/10 @12:00:00",
+                //     "dataRecordCount": "25,000"
+                // }
+            ];
+
+            context.sandbox.utils.each(data, function(index, dataEntry) {
+                var dataHistoryEntry = generateDataHistoryEntryRow(dataEntry);
+                $dataHistoryListTable.append(dataHistoryEntry);
+            });
+            context.$('.data-action-info').on('click', function(event) {
+                exposed.showDetailedInfo({
+                    "datasetId": context.$(this).data('datasetid')
+                });
+            });
         }
     };
+
+    function generateDataHistoryEntryRow(dataHistoryEntryObject) {
+        return dataHistoryEntryTemplate({
+            "datasetId": dataHistoryEntryObject.datasetId,
+            "dataName": dataHistoryEntryObject.dataName,
+            "dataDate": dataHistoryEntryObject.dataDate,
+            "dataRecordCount": dataHistoryEntryObject.dataRecordCount
+        });
+    }
 
     return exposed;
 });
