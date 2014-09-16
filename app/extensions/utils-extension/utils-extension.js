9e1ba50c0a4ad159e086ad4f6f487ce18b99ff56
jrdrgz <jrdrgz89@gmail.com>
2014-09-16 17:44:00 -0400

    End of day commit. Finishing up utils documentation.

--- app/extensions/utils-extension/utils-extension.js
+++ app/extensions/utils-extension/utils-extension.js
@@ -1,18 +1,32 @@
 define([
     'jquery'
 ], function($){
-
+    /**
+      * @exports UtilsExtension
+      */
     var exposed = {
+        /**
+         * Funciton utilized by Meridian to init all extensions. 
+         * This extention exposes {@link Sandbox.utils} to the {@link Sandbox} namespace.
+         * @function
+         * @instance
+         * @param  {Object} app Instance of the Meridian application.
+         */
         initialize: function(app) {
 
             var CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
-            
+            /**
+             * @namespace Sandbox.utils
+             */
             var utils = {
                 /**
-                 * addCSS: 
-                 * @param {string} style - the 'actual' CSS payload
-                 * @param {string} id - optional id for the style element, used so it could be removed later, set as 'data-meridian-style-id'
-                 * example: this.sandbox.utils.addCSS(basemapGalleryCSS, 'basemap-component-style');
+                 * Description for addCSS
+                 * @function
+                 * @instance 
+                 * @param {String} style The 'actual' CSS payload
+                 * @param {String} id Optional id for the style element, used so it could be removed later, set as 'data-meridian-style-id'
+                 * @example this.sandbox.utils.addCSS(basemapGalleryCSS, 'basemap-component-style');
+                 * @memberof Sandbox.utils
                  */
                 addCSS: function(style, id) {
                     var styleId = (id) ? id : '';
@@ -20,8 +34,41 @@ define([
                         .html(style)
                         .appendTo("head");
                 },
-                ajax: $.ajax, // for help see jquery.ajax
+                /**
+                 * Provides access to jQuery's ajax function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.ajax.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                ajax: $.ajax,
+                /**
+                 * Provides access to jQuery's contains function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.contains.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
                 contains: $.contains, // for help see jquery.contains
+                /**
+                 * Creates a geoJson Object based on coordinates provided.
+                 * @function
+                 * @instance 
+                 * @param {Object} coordinates - Coordinates provided.
+                 * @param {String} coordinates.lat - Latitude given.
+                 * @param {String} coordinates.lon - Longitude given.
+                 * @memberof Sandbox.utils
+                 * @return {Object} The geoJson object created.
+                 * @example The return object looks something like this:
+                 * geoJson = {
+                 *     type: 'feature',
+                 *     geometry: {
+                 *         type: 'point',
+                 *         coordinates: [coordinates.lat, coordinates.lon]
+                 *         },
+                 *     properties: {}
+                 * };
+                 */
                 createGeoJson: function(coordinates){
                     var geoJson = {
                             type: 'feature',
@@ -33,15 +80,53 @@ define([
                         };
                     return geoJson;
                 },
-                data: $.data, // for help see jquery.data
-                Deferred: $.Deferred, // for help see jquery.Deferred
-                each: $.each, // for help see jquery.each
-                error: $.error, // for help see jquery.error
-                extend: $.extend, // for help see jquery.extend
                 /**
-                 * first: returns first thing in the item passed in. Works for Objects as well as arrays and strings, but items in obects have NO GUARENTEED ORDER.
-                 * @param  {object/array/string} item - thing you want to look in, to find for first item
-                 * @return {object/array/string} - returns macthing item, or undefined if not found
+                 * Provides access to jQuery's data function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.data.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                data: $.data, 
+                /**
+                 * Provides access to jQuery's Deferred function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.Deferred.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                Deferred: $.Deferred, 
+                /**
+                 * Provides access to jQuery's each function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.each.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                each: $.each, 
+                /**
+                 * Provides access to jQuery's error function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.error.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                error: $.error, 
+                /**
+                 * Provides access to jQuery's extend function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.extend.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                extend: $.extend, 
+                /**
+                 * Return the first value of the input, but items in Objects have NO GUARENTEED ORDER.
+                 * @function
+                 * @instance
+                 * @param  {Object|Array|String} obj - Input to find for first item
+                 * @returns First matching item, or undefined if not found.
+                 * @memberof Sandbox.utils
                  */
                 first: function(obj) {
                     for (var i in obj) {
@@ -51,23 +136,87 @@ define([
                     }
                 },
                 /**
-                 * [forever description]
-                 * @param  {string} selector - string for event type
-                 * @param  {string} data - string to query DOM (tag, class, id)
-                 * @param  {Function} fn - callback to fire on event
-                 * @return {jquery object}
+                 * Create listener on document.
+                 * @function
+                 * @instance
+                 * @param  {String} selector - Event type
+                 * @param  {String} data - Value to query DOM (tag, class, id)
+                 * @param  {Function} fn - Callback function to fire on event
+                 * @memberof Sandbox.utils
                  */
                 forever: function(selector, data, fn){
                     $(document).on(selector, data, fn);
                 },
-                grep: $.grep, // for help see jquery.grep
-                isEmptyObject: $.isEmptyObject, // for help see jquery.isEmptyObject
-                makeArray: $.makeArray, // for help see jquery.makeArray
-                map: $.map, // for help see jquery.map
-                merge: $.merge, // for help see jquery.merge
-                now: $.now, // for help see jquery.now
-                parseJSON: $.parseJSON, // for help see jquery.parseJSON
+                /**
+                 * Provides access to jQuery's grep function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.grep.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                grep: $.grep,
+                /**
+                 * Provides access to jQuery's isEmptyObject function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.isEmptyObject.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                isEmptyObject: $.isEmptyObject,
+                /**
+                 * Provides access to jQuery's makeArray function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.makeArray.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                makeArray: $.makeArray,
+                /**
+                 * Provides access to jQuery's map function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.map.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                map: $.map,
+                /**
+                 * Provides access to jQuery's merge function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.merge.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                merge: $.merge, 
+                /**
+                 * Provides access to jQuery's now function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.now.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                now: $.now, 
+                /**
+                 * Provides access to jQuery's parseJSON function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.parseJSON.
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                parseJSON: $.parseJSON, 
+
+                /**
+                 * A namespace reserved for functions used specificaly for preferences.
+                 * @namespace Sandbox.utils.preferences
+                 */
                 preferences: {
+                    /**
+                     * Description HERE.
+                     * @function
+                     * @instance 
+                     * @param {String} key 
+                     * @memberof Sandbox.utils.preferences
+                     * @return {Object|Boolean} The data found, false if nothing was found. 
+                     */
                     get: function(key){
                         if(typeof(Storage)!=="undefined"){
                             var data;
@@ -83,6 +232,15 @@ define([
                             return false;
                         }
                     },
+                    /**
+                     * For more help...
+                     * @function 
+                     * @instance 
+                     * @param {String} key 
+                     * @param {String | Object} val
+                     * @memberof Sandbox.utils.preferences
+                     * @return {Boolean} True if sucessfully stored, otherwise, false.
+                     */
                     set: function(key, val){
                         if(typeof(Storage)!=="undefined"){
                             if(typeof val === 'object'){
@@ -98,18 +256,46 @@ define([
                     }
                 },
                 /**
-                 * removeCSS: removes a CSS payload (if the one to remove was added through its counterpart method and assigned an id)
-                 * @param  {string} id - 'data-meridian-style-id' of the style element to be removed
-                 * example: this.sandbox.utils.removeCSS(basemap-style');
+                 * Removes a CSS payload (if the one to remove was added through its counterpart method and assigned an id)
+                 * @function
+                 * @instance
+                 * @param  {String} id - 'data-meridian-style-id' of the style element to be removed
+                 * @example this.sandbox.utils.removeCSS('basemap-style');
+                 * @memberof Sandbox.util
                  */
                 removeCSS: function(id) {
                     $('[data-meridian-style-id=' + id + ']').remove();
                 },
-                trim: $.trim, // for help see jquery.trim
-                type: $.type, // for help see jquery.type
+                /**
+                 * Provides access to jQuery's trim function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.trim.
+                 * @function 
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                trim: $.trim,
+                /**
+                 * Provides access to jQuery's type function by exposing it to the {@link Sandbox} namespace.
+                 * For more help, see jquery.type.
+                 * @function 
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
+                type: $.type,
+                /**
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 * @return {String} Internet protocol with the URL host name.
+                 */
                 getCurrentNodeJSEndpoint: function() {
                     return "https://" + window.location.host;
                 },
+                /**
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
                 UUID: function(){
                     var r,
                         rnd= 0,
@@ -131,6 +317,11 @@ define([
                     }
                     return uuid.join("");
                 },
+                /**
+                 * @function
+                 * @instance 
+                 * @memberof Sandbox.utils
+                 */
                 trimNumber: function(number){
                     if(typeof number === "string") {
                         number = parseFloat(number);
