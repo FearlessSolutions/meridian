733462b7748c19802bcc844bbfb432c56fd70aa2
Steven Heinrich <steven.heinrich@gmail.com>
2014-09-22 10:01:16 -0400

    add transformation extension to server

---
+++ server/extensions/transformation/transformation.js
@@ -0,0 +1,58 @@
+var fs = require('fs'),
+    ogr2ogr = require('ogr2ogr')
+    transformationConfig = {
+        "projection": "ESPG:4326",
+        "format": "GeoJSON",
+        "timeout": 15000,
+        "skipFailures": false,
+        "options": []
+    };
+
+exports.toGeoJSON = function(pathToInputFile, pathToOutputFile) {
+    exports.execute({
+        "pathToInputFile": pathToInputFile,
+        "pathToOutputFile": pathToOutputFile,
+        "format": "GeoJSON",
+        "skipFailures": true
+    });
+}
+
+exports.toCSV = function(pathToInputFile, pathToOutputFile) {
+    exports.execute({
+        "pathToInputFile": pathToInputFile,
+        "pathToOutputFile": pathToOutputFile,
+        "format": "CSV",
+        "skipFailures": true
+    });
+}
+
+exports.toKML = function(pathToInputFile, pathToOutputFile) {
+    exports.execute({
+        "pathToInputFile": pathToInputFile,
+        "pathToOutputFile": pathToOutputFile,
+        "format": "KML",
+        "skipFailures": true
+    });
+}
+
+exports.toShapefile = function(pathToInputFile, pathToOutputFile) {
+    exports.execute({
+        "pathToInputFile": pathToInputFile,
+        "pathToOutputFile": pathToOutputFile,
+        "format": "ESRI Shapefile",
+        "skipFailures": true
+    });
+}
+
+exports.execute = function(params) {
+    if(params.pathToInputFile && params.pathToOutputFile) {
+        ogr2ogr(params.pathToInputFile)
+            .project(params.projection || transformationConfig.projection)
+            .format(params.format || transformationConfig.format)
+            .timeout(params.timeout || transformationConfig.timeout)
+            .skipfailures(params.skipFailures || transformationConfig.skipFailures)
+            .options(params.options || transformationConfig.options)
+            .stream();
+            .pipe(fs.createWriteStream(params.pathToOutputFile));
+    }
+}
