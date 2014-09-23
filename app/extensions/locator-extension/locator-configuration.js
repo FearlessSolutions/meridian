9f1ae53c3ca841699bd31d22d14a601292ed2dbd
Steven Heinrich <steven.heinrich@gmail.com>
2014-09-23 14:36:26 -0400

    correct typo in comment

--- app/extensions/locator-extension/locator-configuration.js
+++ app/extensions/locator-extension/locator-configuration.js
@@ -11,8 +11,8 @@ define([
         "dataType": "json",
         "timeout": 7000,
         "noResultsMsg": "No results/suggestions found.",
-        "latIndex": 0, // array index of latitude as it returns from teh geocoding service
-        "lonIndex": 1 // array index of longitude as it returns from teh geocoding service
+        "latIndex": 0, // array index of latitude as it returns from the geocoding service
+        "lonIndex": 1 // array index of longitude as it returns from the geocoding service
     };
 
     return locatorConfiguration;
