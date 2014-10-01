b6bae8f4bc438fc50b99a2a3f3ab44f67fa10d69
Jorge Rodriguez <jrdrgz89@gmail.com>
2014-10-01 18:09:14 -0400

    started external pubsub. Problem with receiveMessage func. End of day.

--- app/extensions/external-pubsub-extension/external-pubsub-extension.js
+++ app/extensions/external-pubsub-extension/external-pubsub-extension.js
@@ -2,8 +2,22 @@ define([
 ], function() {
 
     var callbacks = [];
-
+    /**
+      * @namespace Sandbox.external
+      * @memberof Sandbox
+      */
+    /**
+      * @exports external-pubsub-extension
+      */
     var exposed = {
+        /**
+         * All Meridian extensions require an 'initialize' function to begin the loading process of the extension.
+         * This extension exposes {@link Sandbox.external} to the {@link Sandbox} namespace.
+         * The function adds an even listener on window with "message" as the event. 
+         * @function
+         * @instance
+         * @param {Object} app Instance of the Meridian application.
+         */
         initialize: function(app) {
             window.addEventListener("message", receiveMessage, false); //TODO: third param?
 
@@ -11,10 +25,25 @@ define([
                 app.sandbox.external = {};
             }
 
+            /**
+             * Function description needed.
+             * @function onPostMessage
+             * @instance
+             * @param {Function} callback - Description needed.
+             * @memberof Sandbox.external
+             */
             app.sandbox.external.onPostMessage = function(callback) {
                 callbacks.push(callback);
             };
 
+            /**
+             * Post message to parent: window.opener or window.parent
+             * Message will be sent to '*'.
+             * @function postMessageToParent
+             * @instance
+             * @param  {String} msg - Message to be sent 
+             * @memberof Sandbox.external
+             */
             app.sandbox.external.postMessageToParent = function(msg) {
                 var parent = window.opener || window.parent;
                 parent.postMessage(msg, '*'); // TODO: fix '*'
@@ -22,6 +51,13 @@ define([
         }
     };
 
+    /**
+     * Function description needed.
+     * @function receiveMessage
+     * @instance
+     * @param event - Description needed.
+     * @memberof external-pubsub-extension
+     */
     function receiveMessage(event) {
         callbacks.forEach(function(callback) {
             if (callback && typeof callback === 'function') {
