bd8b6424b13607334235058462c87704a08c2e90
Jorge Rodriguez <jrdrgz89@gmail.com>
2014-10-03 09:59:59 -0400

    Completed fake-extension documentation.

--- app/extensions/fake-extension/fake-extension.js
+++ app/extensions/fake-extension/fake-extension.js
@@ -3,15 +3,48 @@ define([
     'text!./fake-info-win.css',
     'handlebars'
 ], function(fakeHbs, fakeInfoWinCSS) {
+     /**
+     * Sets up fake as a 'dataService'.
+     * @namespace Sandbox.dataServices.fake
+     * @memberof Sandbox.dataServices
+     */
+    /**
+     * Sets up fake as a 'dataService'.
+     * @exports fake-extension
+     */
     var exposed = {
+        /**
+         * All Meridian extensions require an 'initialize' function to begin the loading process of the extension.
+         * This extension exposes {@link Sandbox.dataServices.fake} to the {@link Sandbox} namespace.
+         * @function
+         * @instance
+         * @param {Object} app Instance of the Meridian application.
+         */
         initialize: function(app) {
             app.sandbox.utils.addCSS(fakeInfoWinCSS, 'fake-extension-style');
 
             if (!app.sandbox.dataServices) {
                 app.sandbox.dataServices = {};
             }
+            
             app.sandbox.dataServices.fake = {
+                /**
+                 * Info needed.
+                 * @namespace Sandbox.dataServices.fake.infoWinTemplate
+                 * @memberof Sandbox.dataServices.fake
+                 */
                 "infoWinTemplate": {
+                    /**
+                     * Creates html template with the values provided to the function.
+                     * @function
+                     * @instance
+                     * @param {Object} attributes
+                     * @param {String} attributes.classification
+                     * @param {String} attributes.name
+                     * @param {Object} fullFeature - Used in Sandbox.icons.getIconForFeature and then applied .iconLargeor .icon.
+                     * @return Handlebar template.
+                     * @memberof Sandbox.dataServices.fake.infoWinTemplate
+                     */
                     "buildInfoWinTemplate": function(attributes, fullFeature) {
                         var fakeTemplate = Handlebars.compile(fakeHbs);
                         var html = fakeTemplate({
@@ -23,10 +56,26 @@ define([
                         });
                         return html;
                     },
+                    /**
+                     * NOT IMPLEMENTED. Developers can modify function to execute any post rendering
+                     * action desired after the info window is loaded.
+                     * @function
+                     * @instance
+                     * @param  {Object} feature - Info needed.
+                     * @param  {Object} overlayId - Info needed.
+                     * @memberof Sandbox.dataServices.fake.infoWinTemplate
+                     * @returns return;
+                     */
                     "postRenderingAction": function(feature, layerId) {
                         return;
                     }
                 },
+                /**
+                 * Info needed.
+                 * @namespace Sandbox.dataServices.fake.keys
+                 * @property {String} valid - Property value: "Valid"
+                 * @memberof Sandbox.dataServices.fake
+                 */
                 "keys": {
                     "valid": "Valid"
                 }
