5da1e1f1ed197cb57a938ed3f90847cc66403dc2
Jorge Rodriguez <jrdrgz89@gmail.com>
2014-10-03 11:34:32 -0400

    Finished with icon-extensions and the icons namespace.

--- app/extensions/icon-extension/icon-extension.js
+++ app/extensions/icon-extension/icon-extension.js
@@ -3,11 +3,33 @@ define([
 
     var context,
         currentIcons = {};
-
+    /**
+     * @namespace Sandbox.icons
+     * @memberof Sandbox
+     */
+    /**
+     * @exports icons-extension
+     */
     var exposed = {
+        /**
+         * All Meridian extensions require an 'initialize' function to begin the loading process of the extension.
+         * This extension exposes {@link Sandbox.icons} to the {@link Sandbox} namespace.
+         * @function
+         * @instance
+         * @param {Object} app Instance of the Meridian application.
+         */
         initialize: function(app) {
             context = app;
             app.sandbox.icons = {
+                /**
+                 * Returns the icon style object and stores it internally
+                 * in the {@link module:icons-extension icons-extension}.
+                 * @function
+                 * @instance
+                 * @param  {Object} feature - The feature provided.
+                 * @return {Object} Icon data of the feature provided.
+                 * @memberof Sandbox.icons
+                 */
                 getIconForFeature: function(feature) {
                     var iconData = calculateIcon(feature);
                     if (!currentIcons[iconData.icon]){
@@ -15,9 +37,23 @@ define([
                     }
                     return iconData;
                 },
+                /**
+                 * Gets all current icon styles stored in the {@link module:icons-extension icons-extension}.
+                 * @function
+                 * @instance
+                 * @return {Object} - Internal object of all icon styles.
+                 * @memberof Sandbox.icons
+                 */
                 getCurrentIcons: function() {
                     return currentIcons;
                 },
+                /**
+                 * Clears the interal object of icon styles found in the 
+                 * {@link module:icons-extension icons-extension}.
+                 * @function
+                 * @instance
+                 * @memberof Sandbox.icons
+                 */
                 clearCurrentIcons: function() {
                     currentIcons = {};
                 }
@@ -25,6 +61,15 @@ define([
         }
     };
 
+    /**
+     * Checks if the feature provided has a custom style for its markers.
+     * @function
+     * @instance
+     * @param  {Object} feature - The feature provided.
+     * @return {Object} Custom style if provided, if not, it uses the default markerIcon configuration found
+     * in {@link Sandbox.mapConfiguration}
+     * @memberof module:icons-extension
+     */
     function calculateIcon(feature) {
         if(feature.style) {
             // If a style is provided on the feature, use it
