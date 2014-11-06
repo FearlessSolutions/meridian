405633798f2d046d848ad3fc214cc46bbd7c88b4
Jorge Rodriguez <jrdrgz89@gmail.com>
2014-11-06 17:42:48 -0500

    Finished sessions extension and its namespaces.

--- app/extensions/session-extension/session-extension.js
+++ app/extensions/session-extension/session-extension.js
@@ -3,8 +3,22 @@ define([
 ], function($){
 
     var context;
-
+    /**
+     * Exposes the id of the session to the {@link Sandbox} namespace.
+     * @namespace Sandbox.sessionId
+     * @memberof Sandbox
+     */
+    /**
+     * @exports session-extension
+     */
     var exposed = {
+        /**
+         * All Meridian extensions require an 'initialize' function to begin the loading process of the extension.
+         * This extension sets the current session id and adds custom HTTP headers to the server request.
+         * @function
+         * @instance
+         * @param  {Object} app Instance of the Meridian application.
+         */
         initialize: function(app) {
 
             context = app;
@@ -23,6 +37,13 @@ define([
                 }
             });
         },
+        /**
+         * Applies the session id to {@link Sandbox.sessionId} 
+         * @function
+         * @instance
+         * @param {String} sessionId - The id of the session.
+         * @memberof module:session-extension
+         */
         setSessionId: function(sessionId){
             var id = sessionId.split("-").join("");
             context.sandbox.sessionId = id;
