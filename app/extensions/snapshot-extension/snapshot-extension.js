4db135750bdd7dc7810f7029476e5edd870f347a
Jorge Rodriguez <jrdrgz89@gmail.com>
2014-11-06 17:59:34 -0500

    Finished snapshot extension and its namespaces.

--- app/extensions/snapshot-extension/snapshot-extension.js
+++ app/extensions/snapshot-extension/snapshot-extension.js
@@ -1,13 +1,42 @@
 define([
 ], function() {
-
+    /**
+     * @namespace Sandbox.snapshot
+     * @memberof Sandbox
+     */
+    /**
+     * Exposes the thumbnail returned by {@link module:snapshot-extension#generateThumnailURL generateThumnailURL}.
+     * @namespace Sandbox.snapshot.thumbnailURL
+     * @memberof Sandbox.snapshot
+     */
+    /**
+     * @exports snapshot-extension
+     */
     var exposed = {
+        /**
+         * All Meridian extensions require an 'initialize' function to begin the loading process of the extension.
+         * This extension exposes {@link Sandbox.snapshot} and {@link Sandbox.snapshot.thumbnailURL} to the {@link Sandbox} namespace.
+         * @function
+         * @instance
+         * @param  {Object} app Instance of the Meridian application.
+         */
         initialize: function(app) {
             if (!app.sandbox.snapshot) {
                 app.sandbox.snapshot = {};
             }
             app.sandbox.snapshot.thumbnailURL = exposed.generateThumnailURL;
         },
+        /**
+         * Returns a centered snapshot of the coords provided. 
+         * @function
+         * @instance
+         * @param {Object} coords        - The coordinates provided.
+         * @param {String} coords.MinLat - Minimum Latitude used in the initial extent.
+         * @param {String} coords.MinLon - Minimum Longitude used in the initial extent.
+         * @param {String} coords.MaxLat - Maximum Latitude used in the initial extent.
+         * @param {String} coords.MaxLon - Maximum Longitude used in the initial extent.
+         * @memberof module:snapshot-extension
+         */
         generateThumnailURL: function(coords) {
             if(coords) {
                 var centroidLat = coords.maxLat - (coords.maxLat - coords.minLat) / 2;
