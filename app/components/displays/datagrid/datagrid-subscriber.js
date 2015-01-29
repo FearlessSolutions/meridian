68ebbd169e2b130ab1fc4c8594a8397b0786c6ff
Chris Wilmsen <chris@kindervikings.com>
2015-01-29 09:12:28 -0500

    clear now closes datagrid.

--- app/components/displays/datagrid/datagrid-subscriber.js
+++ app/components/displays/datagrid/datagrid-subscriber.js
@@ -9,7 +9,6 @@ define([
             context.sandbox.on('datagrid.close', datagrid.close);
             context.sandbox.on('datagrid.open', datagrid.open);
             context.sandbox.on('datagrid.reload', datagrid.reload);
-//            context.sandbox.on('map.layer.create', datagrid.reload);
             context.sandbox.on('map.layer.delete', datagrid.reload);
             context.sandbox.on('map.features.plot', datagrid.addData);
             context.sandbox.on('map.features.hide', datagrid.hideFeatures);
