871b0d062bcbfee76befeadfa6f12a5fe08daab2
dale aerni <dale.aerni@synergistcomputing.com>
2015-02-06 15:36:44 -0500

    added focus to input and clear input on change

--- app/components/tools/search/search.js
+++ app/components/tools/search/search.js
@@ -11,56 +11,56 @@ define([
             $inputSearch1 = context.$('#searchAdmin_userid');
             $inputSearch2 = context.$('#searchAdmin_datasource');
             $inputSearch3 = context.$('#searchAdmin_sdate');
-            $inputSearch4 = context.$('#searchAdmin_edate');           
+            $inputSearch4 = context.$('#searchAdmin_edate');
+            $inputGeneric = context.$('input.form-control')
             $toggleSubmit = context.$('#searchAdmin_submit');
             $toggleClear = context.$('#searchAdmin_clear');
             $focusInput = context.$('.form-group .form-control');
+            $searchMsg = context.$('#container-searchmsg');
             var currentInput, currentKey;
             //placeholder for now
             currentKey = 'userId';
                         
             $toggleSearchType.change(function() {
-                $('input.form-control').hide();
+                $inputGeneric.val('').hide();
+                $searchMsg.hide();
                 switch($toggleSearchType.val()) {
                     case "input1val":
-                        $inputSearch1.show();
+                        $inputSearch1.show().focus();
                         currentKey = 'userId';
                         break;
                     case "input2val":
-                        $inputSearch2.show();
+                        $inputSearch2.show().focus();
                         currentKey = 'dataSource';
                         break;
                     case "input3val":
-                        $inputSearch3.show();
-                        currentKey = 'sVal';
+                        $inputSearch3.show().focus();
+                        currentKey = 'createOn';
                         break;
                     case "input4val":
-                       $inputSearch4.show();
+                       $inputSearch4.show().focus();
                         currentKey = 'eVal';
                         break; 
                 }
-
             });
+
+            $focusInput.keyup(function(event) {
+                if($focusInput.filter(function() { return $.trim(this.value) != ''; }).length > 0) {
+                    //There is at least one populated input
+                    $focusInput.removeClass('warning');
+                    $searchMsg.hide();
+                }
+            });    
 
             $toggleSubmit.on('click', function(event) {
 
                 event.preventDefault();
-                currentInput = $('input[type="text"]:visible').val();                
+                currentInput = $.trim($('input[type="text"]:visible').val());
                 var searchSet = {};
-                var dynKey = currentKey;
-                var dynVal = currentInput;
-                searchSet[dynKey] === dynVal;   
-                //console.log(searchSet);
-
+                var dynKey = currentKey;                
+                searchSet[dynKey] = currentInput;   
+                console.log(searchSet);
                 
-                
-                // for(var key in searchSet) {
-                //     // value is empty string
-                //     if(searchSet[key] === '') {
-                //         delete searchSet[key];
-                //     }
-                // }
-
                 var newAJAX = context.sandbox.utils.ajax({
                     // i want
                     // var query =  "SELECT * FROM Queries  WHERE userId LIKE @searchAdmin_userid AND dataSource LIKE @searchAdmin_datasource"
@@ -75,17 +75,14 @@ define([
                 .done(function(data) {
                     if(!currentInput) {                        
                         $('input[type="text"]:visible').addClass('warning');                    
-                        $('#container-searchmsg').show();                    
+                        $searchMsg.show();                    
                     } else {
                         publisher.publisherSearchAdmingridCreate(data);                    
                     };
                 });               
             });
             $toggleClear.on('click', function() { 
-                $('#searchAdmin_userid').val('');
-                $('#searchAdmin_datasource').val('');
-                $('#searchAdmin_sdate').val('');
-                $('#searchAdmin_edate').val('');
+                $inputGeneric.val('');                
             });
             function emptyTest(thingy){                                
                 
