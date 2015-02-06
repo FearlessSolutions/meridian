b5fe885277568f258a0dbe240b0979c3052a97bb
dale aerni <dale.aerni@synergistcomputing.com>
2015-02-06 12:45:32 -0500

    basic implementation of input val working with searchtype toggle

--- app/components/tools/search/search.js
+++ app/components/tools/search/search.js
@@ -8,52 +8,58 @@ define([
         init: function(thisContext) {
             context = thisContext;
             $toggleSearchType = context.$('#searchAdmin_toggleType');
+            $inputSearch1 = context.$('#searchAdmin_userid');
+            $inputSearch2 = context.$('#searchAdmin_datasource');
+            $inputSearch3 = context.$('#searchAdmin_sdate');
+            $inputSearch4 = context.$('#searchAdmin_edate');           
             $toggleSubmit = context.$('#searchAdmin_submit');
             $toggleClear = context.$('#searchAdmin_clear');
             $focusInput = context.$('.form-group .form-control');
-            
+            var currentInput, currentKey;
+            //placeholder for now
+            currentKey = 'userId';
+                        
             $toggleSearchType.change(function() {
                 $('input.form-control').hide();
                 switch($toggleSearchType.val()) {
-                    case "0":
-                        $('#searchAdmin_userid').show();
+                    case "input1val":
+                        $inputSearch1.show();
+                        currentKey = 'userId';
                         break;
-                    case "1":
-                        $('#searchAdmin_userid').show();
+                    case "input2val":
+                        $inputSearch2.show();
+                        currentKey = 'dataSource';
                         break;
-                    case "2":
-                        $('#searchAdmin_userid').show();
+                    case "input3val":
+                        $inputSearch3.show();
+                        currentKey = 'sVal';
                         break;
-                    case "3":
-                        $('#searchAdmin_userid').show();
-                        break;
+                    case "input4val":
+                       $inputSearch4.show();
+                        currentKey = 'eVal';
+                        break; 
                 }
 
             });
 
-            $focusInput.keyup(function(event) {
-                if($focusInput.filter(function() { return $.trim(this.value) != ''; }).length > 0) {
-                    //There is at least one populated input
-                    $focusInput.removeClass('warning');
-                    $('#container-searchmsg').hide();
-                }
-            });            
-
             $toggleSubmit.on('click', function(event) {
-                event.preventDefault();                
-                var val1 = $('#searchAdmin_userid').val(),
-                    val2 = $('#searchAdmin_datasource').val(),
-                    val3 = $('#searchAdmin_sdate').val(),
-                    val4 = $('#searchAdmin_edate').val();
-                                       
-                var searchSet = { userId: val1, dataSource: val2, createdOn: val3, expireOn: val4 };
 
-                for(var key in searchSet) {
-                    // value is empty string
-                    if(searchSet[key] === '') {
-                        delete searchSet[key];
-                    }
-                }
+                event.preventDefault();
+                currentInput = $('input[type="text"]:visible').val();                
+                var searchSet = {};
+                var dynKey = currentKey;
+                var dynVal = currentInput;
+                searchSet[dynKey] === dynVal;   
+                //console.log(searchSet);
+
+                
+                
+                // for(var key in searchSet) {
+                //     // value is empty string
+                //     if(searchSet[key] === '') {
+                //         delete searchSet[key];
+                //     }
+                // }
 
                 var newAJAX = context.sandbox.utils.ajax({
                     // i want
@@ -67,15 +73,12 @@ define([
                     }
                 })
                 .done(function(data) {
-                    // if(!currentField) {  
-                    //     $('#searchAdmin_userid').addClass('warning');
-                    //     $('#searchAdmin_datasource').addClass('warning');
-                    //     $('#searchAdmin_sdate').addClass('warning');
-                    //     $('#searchAdmin_edate').addClass('warning');
-                    //     $('#container-searchmsg').show();
-                    // } else {                        
-                        publisher.publisherSearchAdmingridCreate(data);
-                    // };
+                    if(!currentInput) {                        
+                        $('input[type="text"]:visible').addClass('warning');                    
+                        $('#container-searchmsg').show();                    
+                    } else {
+                        publisher.publisherSearchAdmingridCreate(data);                    
+                    };
                 });               
             });
             $toggleClear.on('click', function() { 
