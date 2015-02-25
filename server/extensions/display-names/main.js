6bffa09a4d23614e701ef2141ed3b67bc105f321
Chris Wilmsen <chris@kindervikings.com>
2015-02-25 15:37:21 -0500

    added display name for upload.

--- server/extensions/display-names/main.js
+++ server/extensions/display-names/main.js
@@ -1,11 +1,13 @@
 var TO_DISPLAY = {
     mock: 'Mock',
-    fake: 'Fake'
+    fake: 'Fake',
+    upload: 'Upload'
 };
 
 var TO_DATASOURCE = {
     Mock: 'mock',
-    Fake: 'fake'
+    Fake: 'fake',
+    Upload: 'upload'
 };
 
 exports.init = function(context){
@@ -16,9 +18,9 @@ exports.init = function(context){
 };
 
 function toDisplay(datasourceName){
-    return TO_DISPLAY[datasourceName];
+    return TO_DISPLAY[datasourceName] || datasourceName;
 }
 
 function fromDisplay(displayName){
-    return TO_DATASOURCE[displayName];
+    return TO_DATASOURCE[displayName] || displayName;
 }
