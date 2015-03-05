f10b5e0830582fae5473ec90585b7a92861e1e16
dale aerni <dale.aerni@synergistcomputing.com>
2015-03-05 12:46:32 -0500

    fix to load all CSS inside main.js of datagrid

--- app/components/displays/datagrid/main.js
+++ app/components/displays/datagrid/main.js
@@ -3,13 +3,19 @@ define([
     './datagrid-subscriber',
     './datagrid-publisher',
 	'text!./datagrid.hbs',
+    'text!../../../libs/SlickGrid-master/slick.grid.css',
+    'text!../../../libs/SlickGrid-master/slick-default-theme.css',
+    'text!../../../libs/SlickGrid-master/controls/slick.pager.css',
 	'text!./datagrid.css',
     'handlebars',
     'jqueryCssWatch'
-], function (component, subscriber, publisher, componentHBS, componentCSS) {
+], function (component, subscriber, publisher, componentHBS, componentCSS, slickgrid_baseCSS, slickgrid_defaultCSS, slickgrid_pagerCSS) {
 
     return {
         initialize: function() {
+            this.sandbox.utils.addCSS(slickgrid_baseCSS, 'slickgrid_base');
+            this.sandbox.utils.addCSS(slickgrid_defaultCSS, 'slickgrid_default')
+            this.sandbox.utils.addCSS(slickgrid_pagerCSS, 'slickgrid_pager')
             this.sandbox.utils.addCSS(componentCSS, 'display-datagrid-component-style');
 
             var datagridTemplate = Handlebars.compile(componentHBS);
