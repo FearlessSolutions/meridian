ba884829f93be1f164fc440ea7f58bd8f05294a8
dale aerni <dale.aerni@synergistcomputing.com>
2015-03-16 16:14:43 -0400

    cleanup of dialog, still using dh data, visually closer

--- app/components/tools/bookmark/bookmark-publisher.js
+++ app/components/tools/bookmark/bookmark-publisher.js
@@ -6,14 +6,23 @@ define([
         init: function(thisContext) {
             context = thisContext;
         },
-        publishOpening: function(params){
-            context.sandbox.emit('menu.opening', params);
-        },
         closeBookmark: function() {
             context.sandbox.emit('bookmark.close');
         },
         openBookmark: function() {
             context.sandbox.emit('bookmark.open');
+        },
+        publishOpening: function(params){
+            context.sandbox.emit('menu.opening', params);
+        },
+        restoreDataset: function(params) {
+            context.sandbox.emit('data.restore', params);
+        },
+        deleteDataset: function(params) {
+            context.sandbox.emit('map.layer.delete', params);
+        },
+        publishMessage: function(params) {
+            context.sandbox.emit('message.publish', params);
         }
     };
 
--- app/components/tools/bookmark/bookmark.js
+++ app/components/tools/bookmark/bookmark.js
@@ -1,16 +1,14 @@
 define([
     './bookmark-publisher',
     'text!./data-history-entry.hbs',
-    'text!./data-history-detail-view.hbs',
     'bootstrap',
     'handlebars',
     'moment'
-], function (publisher, dataHistoryEntryHBS, dataHistoryDetailViewHBS) {
+], function (publisher, dataHistoryEntryHBS) {
 
     var context,
         dataHistoryEntryTemplate,
-        dataHistoryDetailViewTemplate,
-        MENU_DESIGNATION = 'data-history',
+        //dataHistoryDetailViewTemplate,
         $bookmarkModal,
         $bookmarkModalBody,
         $bookmarkCloseButton,
@@ -25,14 +23,14 @@ define([
             context = thisContext;
 
             dataHistoryEntryTemplate = Handlebars.compile(dataHistoryEntryHBS);
-            dataHistoryDetailViewTemplate = Handlebars.compile(dataHistoryDetailViewHBS);
+            //dataHistoryDetailViewTemplate = Handlebars.compile(dataHistoryDetailViewHBS);
 
             $bookmarkModal = context.$('#bookmark-modal');
             $bookmarkModalBody = context.$('#bookmark-modal .modal-body');
             $bookmarkCloseButton = context.$('#bookmark-modal.modal button.close');
 
             $dataHistoryListTable = context.$('#bookmark-modal.modal .data-history-list');
-            $dataHistoryDetailView = context.$('#bookmark-modal.modal .data-history-detail-view');
+            //$dataHistoryDetailView = context.$('#bookmark-modal.modal .data-history-detail-view');
             $noDataLabel = context.$('#bookmark-modal.modal p.noDataLabel');
 
             $bookmarkModal.modal({
@@ -57,71 +55,7 @@ define([
         closeBookmark: function() {
             $bookmarkModal.modal('hide');
         },
-        showDetailedInfo: function(dataObject) {
-            var newAJAX = context.sandbox.utils.ajax({
-                type: 'GET',
-                url: context.sandbox.utils.getCurrentNodeJSEndpoint() + '/metadata/query/' + dataObject.datasetId,
-                xhrFields: {
-                    withCredentials: true
-                }
-            })
-            .done(function(data) {
-                var now = moment(), //This needs to be done now to prevent race condition later
-                    dataDate = moment.unix(data.createdOn),
-                    expireDate = moment.unix(data.expireOn),
-                    isExpired = expireDate.isBefore(now),
-                    disableRestore = isExpired, //Use this as default
-                    tempData,
-                    rawDataObjectString,
-                    dataHistoryDetailView,
-                    dataStatus = isExpired ? 'Expired' : 'N/A';
 
-                if(context.sandbox.stateManager.layers[data.queryId]){
-                    disableRestore = true;
-                }
-
-                tempData = {
-                    datasetId: data.queryId,
-                    dataSessionId: data.sessionId,
-                    dataSource: data.dataSource || 'N/A',
-                    dataName: data.queryName || 'N/A',
-                    dataDate: dataDate.format('MMMM Do YYYY, h:mm:ss a') || 'N/A',
-                    dataRecordCount: data.numRecords || 'N/A',
-                    dataExpiresOn: expireDate.format('MMMM Do YYYY, h:mm:ss a') || 'N/A',
-                    disableRestore: disableRestore,
-                    dataStatus: dataStatus,
-                    rawDataObject: data.rawQuery || 'N/A'
-                };
-
-                rawDataObjectString = context.sandbox.utils.isEmptyObject(tempData.rawDataObject) ? '' : JSON.stringify(tempData.rawDataObject, null,  ' ');
-                tempData.rawDataObject = rawDataObjectString;
-
-                dataHistoryDetailView = dataHistoryDetailViewTemplate(tempData);
-                $dataHistoryDetailView.html(dataHistoryDetailView);
-
-                context.$('.data-history-detail-view .data-history-modal-back-to-list').on('click', function(event) {
-                    exposed.hideDetailedInfo();
-                });
-                context.$('.data-history-detail-view .data-action-restore').on('click', function(event) {
-                    publisher.restoreDataset(data);
-                    publisher.closeBookmark();
-                });
-                context.$('.data-history-detail-view .data-action-delete').on('click', function(event) {
-                    // Delete the dataset
-                    deleteDataset(tempData.datasetId, tempData.dataSessionId);
-                    exposed.hideDetailedInfo();
-                });
-
-                $modalBody.addClass('finiteHeight');
-                context.$('.data-history-summary-list-container').addClass('hidden');
-                context.$('.data-history-detail-view').removeClass('hidden');
-            });
-        },
-        hideDetailedInfo: function() {
-            $modalBody.removeClass('finiteHeight');
-            context.$('.data-history-summary-list-container').removeClass('hidden');
-            context.$('.data-history-detail-view').addClass('hidden');
-        },
         clear: function() {
             $bookmarkModal.modal('hide');
         },
@@ -185,14 +119,16 @@ define([
             $noDataLabel.addClass('hide');
         }
 
-        context.$('.data-history-list .data-action-info').on('click', function(event) {
-            exposed.showDetailedInfo({
-                datasetId: context.$(this).parent().parent().data('datasetid')
-            });
-        });
         context.$('.data-history-list .data-action-restore').on('click', function(event) {
             publisher.restoreDataset(currentDataSet[context.$(this).parent().parent().data('datasetid')]);
             publisher.closeBookmark();
+        });
+        context.$('.data-history-list .data-action-edit').on('click', function(event) {
+            //exposed.showDetailedInfo({
+            //    datasetId: context.$(this).parent().parent().data('datasetid')
+            //});
+            console.log(context.$(this).parent().parent().data('datasetid'));
+            console.log(context.$(this).parent().parent().data('datasessionid'));
         });
         context.$('.data-history-list .data-action-delete').on('click', function(event) {
             // Delete the dataset
@@ -253,7 +189,7 @@ define([
             populateDataHistoryTable();
             publisher.publishMessage( {
                 messageType: 'success',
-                messageTitle: 'Data History',
+                messageTitle: 'Bookmarks',
                 messageText: 'Dataset successfully removed'
             });
         });
