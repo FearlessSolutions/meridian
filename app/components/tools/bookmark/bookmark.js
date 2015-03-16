ffb14353fe25a4049488b551433f7532a6d98849
dale aerni <dale.aerni@synergistcomputing.com>
2015-03-15 20:36:34 -0400

    viewport fix and buttonFormatter prototype

--- app/components/tools/bookmark/bookmark.js
+++ app/components/tools/bookmark/bookmark.js
@@ -12,7 +12,7 @@ define([
     var context,
         $bookmarkModal,
         $bookmarkModalBody,
-        $bookmarkCloseButton
+        $bookmarkCloseButton,
         grid,
         data,
         dataView, columns, options;
@@ -38,6 +38,46 @@ define([
                 publisher.closeBookmark();
             });
 
+            var columns = [
+                {id: "col1", name: "Name", field: "bname", minWidth: 155 },
+                {id: "col2", name: "Max Lat", field: "maxLat" },
+                {id: "col3", name: "Min Lat", field: "minLat" },
+                {id: "col4", name: "Max Lon", field: "maxLon" },
+                {id: "col5", name: "Min Lon", field: "minLon" },
+                {id: "col6", name: "test col", field: "testCol", minWidth:85, formatter: buttonSet }
+
+            ];
+            var options = {
+                enableCellNavigation: true,
+                enableColumnReorder: false,
+                //forceFitColumns: true,
+                fullWidthRows: true
+            };
+
+            var data = [];
+            for (var i = 0; i < 500; i++) {
+                data[i] = {
+                    bname: "Bookmarkname " + i,
+                    maxLat: "Max Lat " + i,
+                    minLat: "Min Lat " + i,
+                    maxLon: "Max Lon " + i,
+                    minLon: "Min Lon " + i
+                };
+            }
+            grid = new Slick.Grid("#bmgrid", data, columns, options);
+
+            $(window).resize(function(){
+                // redraws grid on browser resize
+                grid.resizeCanvas();
+                gridHeight();
+            });
+            //$(window).resize();
+            gridHeight();
+            grid.resizeCanvas();
+            gridHeight();
+
+
+
         },
         openBookmark: function() {
             $bookmarkModal.modal('show');
@@ -45,11 +85,23 @@ define([
         closeBookmark: function() {
             $bookmarkModal.modal('hide');
         },
-
         clear: function() {
             $bookmarkModal.modal('hide');
         }
     };
+
+    function gridHeight () {
+        $('#bmgrid').height(300).width(650);
+        $('.slick-viewport').height(271);
+    }
+    function buttonSet(row,cell,value,columnDef,dataContext) {
+        var button = "<button title='button1' class='bm1' type='button' id='"+ dataContext.id +"' />" +
+            "<button title='button2' class='bm2' type='button' />" +
+            "<button title='button3' class='bm3' type='button' />";
+        //the id is so that you can identify the row when the particular button is clicked
+        return button;
+
+    }
 
     return exposed;
 
