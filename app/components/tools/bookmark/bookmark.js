4f00e9000506f9b9f865f6631ce53d5fd12bd5b5
dale aerni <dale.aerni@synergistcomputing.com>
2015-03-24 15:47:19 -0400

    move row delete to function and call grid refresh instead

--- app/components/tools/bookmark/bookmark.js
+++ app/components/tools/bookmark/bookmark.js
@@ -19,12 +19,10 @@ define([
             context = thisContext;
 
             bookmarkEntryTemplate = Handlebars.compile(bookmarkEntryHBS);
-
             $bookmarkModal = context.$('#bookmark-modal');
             $bookmarkModalBody = context.$('#bookmark-modal .modal-body');
             $bookmarkCloseButton = context.$('#bookmark-modal.modal button.close');
-
-            $bookmarkListTable = context.$('#bookmark-modal.modal .data-history-list');
+            $bookmarkListTable = context.$('#bookmark-modal.modal .bookmark-list');
             $noDataLabel = context.$('#bookmark-modal.modal p.noDataLabel');
 
             $bookmarkModal.modal({
@@ -69,42 +67,34 @@ define([
                 minLon: 'somevalue'
             };
             context.sandbox.utils.preferences.set('storedBookmarks', storedBookmarks);
-            console.log(localStorage);
         },
         updateBookmarks: function() {
             var bmData = JSON.parse(localStorage.getItem("storedBookmarks"));
             $bookmarkListTable.empty();
+            checkStored(bmData);
 
-            if ( bmData === null  || Object.keys(bmData).length == 0 )  {
-                $noDataLabel.removeClass('hide');
-            } else {
-                context.sandbox.utils.each(bmData, function (index, tempDataEntry) {
-                    var bookmarkEntry = generateDataHistoryEntryRow(tempDataEntry);
-                    $bookmarkListTable.append(bookmarkEntry);
-                });
-                $noDataLabel.addClass('hide');
-            }
+            context.sandbox.utils.each(bmData, function (index, tempDataEntry) {
+                var bookmarkEntry = generateBookmarkEntryRow(tempDataEntry);
+                $bookmarkListTable.append(bookmarkEntry);
+            });
 
-            context.$('.data-history-list .data-action-restore').on('click', function(event) {
+            context.$('.bookmark-list .data-action-restore').on('click', function(event) {
                 publisher.restoreDataset(something[context.$(this).parent().parent().data('datasetid')]);
                 publisher.closeBookmark();
             });
-            context.$('.data-history-list .data-action-edit').on('click', function(event) {
-
+            context.$('.bookmark-list .data-action-edit').on('click', function(event) {
                 var bmData = JSON.parse(localStorage.getItem("storedBookmarks"));
-
                 console.log(bmData);
             });
-            context.$('.data-history-list .data-action-delete').on('click', function(event) {
+            context.$('.bookmark-list .data-action-delete').on('click', function(event) {
                 // Delete the bookmark
-                deleteDataset(context.$(this).parent().parent().data('bmid'));
-                $(this).parent().parent('tr').remove();
+                deleteBookmark(context.$(this).parent().parent().data('bmid'), context.$(this).parent().parent('tr'));
+                exposed.updateBookmarks();
             });
-
         }
     };
 
-    function generateDataHistoryEntryRow(bookmarkEntryObject) {
+    function generateBookmarkEntryRow(bookmarkEntryObject) {
         return bookmarkEntryTemplate({
             bmId: bookmarkEntryObject.bmId,
             name: bookmarkEntryObject.bmName,
@@ -112,12 +102,19 @@ define([
         });
     }
 
-    function deleteDataset(bookmarkId) {
+    function checkStored(bmData) {
+        if ( bmData === null  || Object.keys(bmData).length == 0 )  {
+            $noDataLabel.removeClass('hide');
+        } else {
+            $noDataLabel.addClass('hide');
+        };
+    }
+
+    function deleteBookmark(bookmarkId, rowToDelete) {
         var storedBookmarks = context.sandbox.utils.preferences.get('storedBookmarks');
         delete storedBookmarks[bookmarkId];
         context.sandbox.utils.preferences.set('storedBookmarks', storedBookmarks);
-
-        console.log(localStorage);
+        rowToDelete.remove();
 
         publisher.publishMessage( {
             messageType: 'success',
