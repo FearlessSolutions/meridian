fd34c6e26ac8a5dedeb1b149ce6b0f061ef4675c
Jorge Rodriguez <jrdrgz89@gmail.com>
2015-03-31 18:00:53 -0400

    Adding leaflet files following renderer structure.

---
+++ app/components/rendering-engines/leaflet/main.js
@@ -0,0 +1,49 @@
+define([], function (){
+    var exposed = {
+        initialize: function() {
+            if(this.sandbox.mapConfiguration.defaultMapEngine === 'Leaflet') {
+                requireStuff(this);
+            }
+
+            this.sandbox.on('map.renderer.change', function(params) {
+                if(params.provider === 'Leaflet') {
+                    requireStuff(this);
+                } else {
+                    // Destroy OL Map
+                }
+            });
+        }
+    };
+
+    function requireStuff(thisContext) {
+        var context = thisContext;
+        
+        //Require modules needed for OpenLayers
+        require([
+            './components/rendering-engines/map-openlayers/map/core',
+            'text!./components/rendering-engines/map-openlayers/map-openlayers.css', 
+            'text!./components/rendering-engines/map-openlayers/map-openlayers.hbs',
+            './components/rendering-engines/map-openlayers/map-api-publisher',
+            './components/rendering-engines/map-openlayers/map-api-subscriber',
+            'handlebars'
+        ], function(
+            mapCore,
+            olMapRendererCSS,
+            olMapRendererHBS,
+            olMapRendererPublisher,
+            olMapRendererSubscriber
+        ){
+            context.sandbox.utils.addCSS(olMapRendererCSS, 'rendering-engines-map-openlayers-component-style');
+
+            var olMapRendererTemplate = Handlebars.compile(olMapRendererHBS);
+            var html = olMapRendererTemplate();
+            context.html(html);
+
+            mapCore.init(context);
+            olMapRendererPublisher.init(context);
+            olMapRendererSubscriber.init(context);
+        });
+    }
+
+    return exposed;
+});
