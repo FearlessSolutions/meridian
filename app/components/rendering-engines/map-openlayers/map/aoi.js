0757396355a1331ebf7e8f99ab2f6411654932e5
John Foster <jfoster@fearsol.com>
2015-04-08 10:20:38 -0400

    added aoi file to rendering engine

---
+++ app/components/rendering-engines/map-openlayers/map/aoi.js
@@ -0,0 +1,80 @@
+define([
+], function (publisher) {
+    var context;
+
+    var exposed = {
+        init: function(thisContext) {
+            context = thisContext;
+        },
+        createAOI: function(args){
+            var layerId = args.layerId,
+                name = args.name,
+                coords = args.coords;
+
+            if(!layerId ||
+                !context.sandbox.dataStorage.datasets[layerId] ||
+                !coords){
+                return;
+            }
+
+            //This should be ignored if the layer exists (handled in renderer)
+            publisher.createLayer({
+                "layerId": layerId + "_aoi",
+                "name": name + "_aoi",
+                "initialVisibility": true,
+                "styleMap": {
+                    "default": {
+                        "strokeColor": '#000',
+                        "strokeOpacity": 0.3,
+                        "strokeWidth": 2,
+                        "fillColor": 'gray',
+                        "fillOpacity": 0.3
+                    }
+                }
+            });
+
+            if(coords){
+                createNewAOIFeature(layerId, coords);
+            }
+        },
+        updateAOI: function(args){
+            var layerId = args.layerId,
+                coords = args.coords;
+
+            if(!layerId ||
+                !context.sandbox.dataStorage.datasets[layerId] ||
+                !coords){
+                return;
+            }
+
+            createNewAOIFeature(layerId, coords);
+        },
+        clear: function() {
+           //TODO?
+        }
+    };
+
+    function createNewAOIFeature(layerId, coords){
+        publisher.publishUpdateData({
+            "layerId": layerId + "_aoi",
+            "data": [{
+                "layerId": layerId + "_aoi",
+                "featureId": "_aoi",
+                "dataService": "",
+                "id": "_aoi",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [[
+                        [coords.minLon, coords.maxLat],
+                        [coords.maxLon, coords.maxLat],
+                        [coords.maxLon, coords.minLat],
+                        [coords.minLon, coords.minLat]
+                    ]]
+                },
+                "type": "Feature"
+            }]
+        });
+    }
+
+    return exposed;
+});
