ef1c0aab2ee04a6cb37e15bd629bf6fa9390cccc
John Foster <jfoster@fearsol.com>
2015-04-20 14:22:09 -0400

    fixed query toggle

--- app/components/controls/query-toggle/query-toggle-mediator.js
+++ app/components/controls/query-toggle/query-toggle-mediator.js
@@ -9,6 +9,12 @@ define([
             context.sandbox.on('query.tool.close', queryToggle.removeActive);
             context.sandbox.on('query.tool.open', queryToggle.setActive);
             context.sandbox.on('data.clear.all', queryToggle.clear);
+        },
+        closeQueryTool: function(params) {
+            context.sandbox.emit('query.tool.close');
+        },
+        openQueryTool: function(params) {
+            context.sandbox.emit('query.tool.open');
         }
     };	
 
