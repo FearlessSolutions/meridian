638c305e89b82a9ddb06a1778657a8aa2ee2896e
John Foster <jfoster@fearsol.com>
2015-04-20 14:16:21 -0400

    fixed playback

--- app/components/controls/playback/playback-mediator.js
+++ app/components/controls/playback/playback-mediator.js
@@ -8,6 +8,15 @@ define([
             context = thisContext;
             context.sandbox.on('timeline.playback.start', playback.startPlayback);
             context.sandbox.on('timeline.playback.stop', playback.stopPlayback);
+        },
+        startPlayback: function(params){
+            context.sandbox.emit('timeline.playback.start', params);
+        },
+        stopPlayback: function(params){
+            context.sandbox.emit('timeline.playback.stop', params);
+        },
+        publishMessage: function(params) {
+            context.sandbox.emit('message.publish', params); 
         }
     };
 
