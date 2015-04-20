da4d25be7eb4e9a05c8acbac55c1f652de0da5b8
John Foster <jfoster@fearsol.com>
2015-04-20 14:10:26 -0400

    fixed legend toggle

--- app/components/controls/legend-toggle/legend-toggle-mediator.js
+++ app/components/controls/legend-toggle/legend-toggle-mediator.js
@@ -10,6 +10,12 @@ define([
             context.sandbox.on('map.legend.show', legendToggle.addActiveClass);
             context.sandbox.on('map.legend.hide', legendToggle.removeActiveClass);
             context.sandbox.on('data.clear.all', legendToggle.removeActiveClass);
+        },
+        showLegend: function(params) {
+            context.sandbox.emit('map.legend.show', params);
+        },
+        hideLegend: function(params) {
+            context.sandbox.emit('map.legend.hide', params);
         }
     };	
 
