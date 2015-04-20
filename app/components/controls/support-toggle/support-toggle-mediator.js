21c7f0d874dab2bf8438e64943d068f085811000
John Foster <jfoster@fearsol.com>
2015-04-20 14:37:33 -0400

    fixed support toggle

--- app/components/controls/support-toggle/support-toggle-mediator.js
+++ app/components/controls/support-toggle/support-toggle-mediator.js
@@ -1,11 +1,14 @@
 define([
-], function(){
-
+    './support-toggle'
+], function (supportToggle) {
     var context;
 
     var exposed = {
         init: function(thisContext) {
             context = thisContext;
+            context.sandbox.on('support.close', supportToggle.removeActive);
+            context.sandbox.on('support.open', supportToggle.setActive);
+            context.sandbox.on('data.clear.all', supportToggle.clear);
         },
         closeSupport: function(params) {
             context.sandbox.emit('support.close');
@@ -13,8 +16,7 @@ define([
         openSupport: function(params) {
             context.sandbox.emit('support.open');
         }
-    };
+    };	
 
     return exposed;
-
 });
