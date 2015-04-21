6c88725be49194e4233efc4835ac9e71e3a9145a
John Foster <jfoster@fearsol.com>
2015-04-21 15:43:46 -0400

    fixed clear

--- app/components/displays/clear/clear-mediator.js
+++ app/components/displays/clear/clear-mediator.js
@@ -1,11 +1,13 @@
 define([
-], function () {
-
+	'./clear'
+], function (clear) {
     var context;
 
-    var exposed = {
-        init: function(thisContext) {
+	var exposed = {
+        init: function(thisContext){
             context = thisContext;
+            context.sandbox.on('clear.menu.open', clear.open);
+            context.sandbox.on('data.clear.all', clear.clear);
         },
         publishClear: function(){
             context.sandbox.emit('data.clear.all');
@@ -13,8 +15,7 @@ define([
         publishOpening: function(params){
             context.sandbox.emit('menu.opening', params);
         }
-    };
+    };	
 
     return exposed;
-
 });
