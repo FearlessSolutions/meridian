23c8201343c6f12c0b47fea0c7c3ba1d7a19dbef
John Foster <jfoster@fearsol.com>
2015-04-21 13:27:53 -0400

    fixed data upload

--- app/components/controls/upload-data-toggle/data-upload-toggle-mediator.js
+++ app/components/controls/upload-data-toggle/data-upload-toggle-mediator.js
@@ -9,6 +9,12 @@ define([
             context.sandbox.on('tool.upload.show', toggle.addActiveClass);
             context.sandbox.on('tool.upload.hide', toggle.removeActiveClass);
             context.sandbox.on('data.clear.all', toggle.removeActiveClass);
+        },
+        showLegend: function(params) {
+            context.sandbox.emit('tool.upload.show', params);
+        },
+        hideLegend: function(params) {
+            context.sandbox.emit('tool.upload.hide', params);
         }
     };	
 
