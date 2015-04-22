54a80201289c84c82f57c67cf6657865e65fb5de
John Foster <jfoster@fearsol.com>
2015-04-22 16:18:25 -0400

    fixed boilerplate

--- app/components/_boilerplate/boilerplate-mediator.js
+++ app/components/_boilerplate/boilerplate-mediator.js
@@ -11,6 +11,10 @@ define([
             // The 'on' function subscribes to the specified channel and calls dummyFunction
             // If there are params passed along the channel, then those are passed to dummyFunction
             context.sandbox.on('map.zoom.in', boilerplate.dummyFunction);
+        },
+        publishMessage: function(params) {
+            // The 'emit' function is used to publish to the specified channel with a payload of params
+            context.sandbox.emit('message.publish', params); 
         }
     };	
 
