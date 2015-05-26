f3d67aa37d8fc042fd3c8c3ea72da62dfd34e3b7
dale aerni <dale.aerni@synergistcomputing.com>
2015-05-26 14:28:58 -0400

    for chris#1

--- app/components/apis/cmapi/feature/cmapi-feature.js
+++ app/components/apis/cmapi/feature/cmapi-feature.js
@@ -67,9 +67,14 @@ define([
         var layerId = message.overlayId || context.sandbox.cmapi.defaultLayerId,
             featureIndex,
             fId,
-            sessionId = context.sandbox.sessionId;
+            sessionId = context.sandbox.sessionId,
+            DATASOURCE_NAME = 'cmapi',
+            //new
+            newData = [],
+            keys = context.sandbox.cmapi.keys,
+            newKeys = {};
 
-        //check if layer exsist, if not create it
+        //check if layer exist, if not create it
         if(!context.sandbox.dataStorage.datasets[layerId]) {
             //create new layer with overlayId provided
 
@@ -79,28 +84,90 @@ define([
 
             mediator.createLayer({
                 layerId: layerId,
-                selectable: false //TODO remove when select is re-implemented
+                //selectable: false //TODO remove when select is re-implemented
+                // new
+                name: message.name || layerId,
+                selectable: true,
+                coords: message.coords
             });    
         }
 
+        context.sandbox.stateManager.setLayerStateById({
+            layerId: layerId,
+            state: {
+                dataTransferState: 'running'
+            }
+        });
+
         featureIndex = message.feature.features;
 
-        context.sandbox.utils.each(featureIndex, function(index, feature) {
+        context.sandbox.utils.each(featureIndex, function(dataindex, feature) {
             if(!feature.properties){
                 feature.properties = {};
             }
             fId = feature.properties.featureId || context.sandbox.utils.UUID();
             fId += sessionId;
+            var newValue = {
+                // original
+                //fId = feature.properties.featureId || context.sandbox.utils.UUID();
+                //fId += sessionId;
+                //
+                //feature.id = fId;
+                //feature.properties.featureId = fId;
+                //feature.dataService = context.sandbox.cmapi.DATASOURCE_NAME;
+                dataService: context.sandbox.cmapi.DATASOURCE_NAME,
+                layerId: message.overlayId,
+                id: fId,
+                geometry: feature.geometry,
+                type: feature.type,
+                properties: feature.properties,
+                lat: feature.geometry.coordinates[1],
+                lon: feature.geometry.coordinates[0],
+                featureId: fId
+            }
+            //console.debug(newValue);
 
-            feature.id = fId;
-            feature.properties.featureId = fId;
-            feature.dataService = context.sandbox.cmapi.DATASOURCE_NAME;
+            if(keys){
+                //For each of the keys required, if that property exists in the feature, hoist it
+                //and give it the specified header name
+                context.sandbox.utils.each(keys, function(index, keyMetadata){
+                    if(feature.properties[keyMetadata.property] !== undefined){
+                        newValue[keyMetadata.property] = feature.properties[keyMetadata.property];
+                        if(!newKeys[keyMetadata.property]){
+                            newKeys[keyMetadata.property] = keyMetadata;
+                        }
+                    }
+                });
+            }
+
+            //featureIndex[dataindex].dataService = 'cmapi';
+
+            context.sandbox.dataStorage.addData({
+                datasetId: layerId,
+                data: newValue
+            });
+
+            // Add style properties for map features, but not for local dataset storage
+            context.sandbox.utils.each(context.sandbox.icons.getIconForFeature(feature), function(styleKey, styleValue){
+                newValue.properties[styleKey] = styleValue;
+            });
+
+            newData.push(newValue);
         });
+
+        //Add new keys for the datagrid
+        context.sandbox.dataStorage.insertKeys({
+            keys: newKeys
+        });
+
+        // Clear data out from memory
+        message = [];
 
         //plot feature(s) from payload
         mediator.plotFeatures({
             layerId: layerId,
-            data: message.feature.features
+            //data: message.feature.features
+            data: newData
         });    
 
         //zoom to feature if specified in payload
