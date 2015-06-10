ef52f5c710a3b83361717a7684a38e0040985b66
sethmussey <sethmussey@gmail.com>
2015-06-10 17:42:36 -0400

    Post Commented Out.

--- app/components/controls/timeline/timeline.js
+++ app/components/controls/timeline/timeline.js
@@ -31,45 +31,6 @@ define([
 
                     thumnailURL = context.sandbox.snapshot.thumbnailURL(coords);
 
-                    //mediator.createLayer({
-                    //    layerId: layerId + '_aoi',
-                    //    name: name + '_aoi',
-                    //    initialVisibility: true,
-                    //    styleMap: {
-                    //        default: {
-                    //            strokeColor: '#000',
-                    //            strokeOpacity: 0.3,
-                    //            strokeWidth: 2,
-                    //            fillColor: 'gray',
-                    //            fillOpacity: 0.3
-                    //        }
-                    //    }
-                    //});
-                    //
-                    //mediator.setLayerIndex({
-                    //    layerId: layerId + '_aoi',
-                    //    layerIndex: 0
-                    //});
-                    //
-                    //mediator.plotFeatures({
-                    //    layerId: layerId + '_aoi',
-                    //    data: [{
-                    //        layerId: layerId + '_aoi',
-                    //        featureId: '_aoi',
-                    //        dataService: '',
-                    //        id: '_aoi',
-                    //        geometry: {
-                    //            type: 'Polygon',
-                    //            coordinates: [[
-                    //                [coords.minLon, coords.maxLat],
-                    //                [coords.maxLon, coords.maxLat],
-                    //                [coords.maxLon, coords.minLat],
-                    //                [coords.minLon, coords.minLat]
-                    //            ]]
-                    //        },
-                    //        type: 'Feature'
-                    //    }]
-                    //});
 
                 } else {
                     thumnailURL = context.sandbox.snapshot.thumbnailURL();
@@ -101,16 +62,10 @@ define([
                         exposed.showDataLayer({
                             layerId: layerId
                         });
-                        //exposed.showAOILayer({
-                        //    layerId: layerId
-                        //});
                     } else {
                         exposed.hideDataLayer({
                             layerId: layerId
                         });
-                        //exposed.hideAOILayer({
-                        //    layerId: layerId
-                        //});
                     }
                 });
 
@@ -278,10 +233,7 @@ define([
         },
         showLayer: function(params) {
             if(context.sandbox.dataStorage.datasets[params.layerId]) {
-                // Take care of AOI and toggleBtn state
-                //exposed.showAOILayer({
-                //    layerId: params.layerId
-                //});
+                // Take care of toggleBtn state
                 exposed.layerToggleOn({
                     layerId: params.layerId
                 });
@@ -289,10 +241,7 @@ define([
         },
         hideLayer: function(params) {
             if(context.sandbox.dataStorage.datasets[params.layerId]) {
-                // Take care of AOI and toggleBtn state
-                //exposed.hideAOILayer({
-                //    layerId: params.layerId
-                //});
+                // Take care of toggleBtn state
                 exposed.layerToggleOff({
                     layerId: params.layerId
                 });
@@ -301,11 +250,6 @@ define([
         deleteLayer: function(params) {
             var layerId = params.layerId;
             if(context.$('#snapshot-' + layerId).length){
-
-                // Take care of AOI and toggleBtn state
-                //exposed.deleteAOILayer({
-                //    layerId: layerId
-                //});
                 exposed.deleteSnapshot({
                     layerId: layerId
                 });
@@ -319,9 +263,6 @@ define([
             exposed.showDataLayer({
                 layerId: params.layerId
             });
-            //exposed.showAOILayer({
-            //    layerId: params.layerId
-            //});
             exposed.layerToggleOn({
                 layerId: params.layerId
             });
@@ -330,9 +271,6 @@ define([
             exposed.hideDataLayer({
                 layerId: params.layerId
             });
-            //exposed.hideAOILayer({
-            //    layerId: params.layerId
-            //});
             exposed.layerToggleOff({
                 layerId: params.layerId
             });
@@ -344,9 +282,6 @@ define([
             exposed.deleteDataLayer({
                  layerId: params.layerId
             });
-            //exposed.deleteAOILayer({
-            //     layerId: params.layerId
-            //});
         },
         showDataLayer: function(params) { //TODO this needs to be cleaned up. No reason to have multiple instnaces of the same logic.
             context.sandbox.stateManager.layers[params.layerId].visible = true;
@@ -366,24 +301,7 @@ define([
             });
             mediator.deleteLayer({layerId: params.layerId});
         },
-        //showAOILayer: function(params) {
-        //    if(context.sandbox.stateManager.layers[params.layerId + '_aoi']) {
-        //        context.sandbox.stateManager.layers[params.layerId + '_aoi'].visible = true;
-        //        mediator.showLayer({layerId: params.layerId + '_aoi'});
-        //    }
-        //},
-        //hideAOILayer: function(params) {
-        //    if(context.sandbox.stateManager.layers[params.layerId + '_aoi']) {
-        //        context.sandbox.stateManager.layers[params.layerId + '_aoi'].visible = false;
-        //        mediator.hideLayer({layerId: params.layerId + '_aoi'});
-        //    }
-        //},
-        //deleteAOILayer: function(params) {
-        //    var layerId = params.layerId;
-        //
-        //    delete context.sandbox.stateManager.layers[layerId + '_aoi'];
-        //    mediator.deleteLayer({layerId: layerId + '_aoi'});
-        //},
+
         layerToggleOn: function(params) {
             var $querySnapshot = context.$('#snapshot-' + params.layerId);
             $querySnapshot.find('.btn-on').addClass('btn-primary');
